a0cb3e548d459f4eea5dc599e8d01734bb5ba654
Abhay Singh <abhay5349singh@gmail.com>
2022-08-22 23:48:07 +0530

    Update 1182. Shortest Distance to Target Color.cpp

--- Algorithm 3/1182. Shortest Distance to Target Color.cpp
+++ Algorithm 3/1182. Shortest Distance to Target Color.cpp
@@ -1,5 +1,7 @@
 Question Link: https://leetcode.com/problems/shortest-distance-to-target-color/
 
+// Approach 1
+
 class Solution {
 public:
     
@@ -23,6 +25,53 @@ public:
             if(colors[i]==1) one.insert(i), c1=true;
             if(colors[i]==2) two.insert(i), c2=true;
             if(colors[i]==3) three.insert(i), c3=true;
+        }
+        
+        vector<int> ans;
+        for(int i=0;i<queries.size();i++){
+            int idx=queries[i][0], c=queries[i][1];
+            
+            int d=n+1;
+            if(c1 && c==1) distance(one, idx, d);
+            if(c2 && c==2) distance(two, idx, d);
+            if(c3 && c==3) distance(three, idx, d);
+            
+            ans.push_back((d==n+1 ? -1:d));
+        }
+        return ans;
+    }
+};
+
+// Approach 2:
+class Solution {
+public:
+    
+    int distance(vector<int> v, int idx, int &d){
+        int l=0, r=v.size()-1;
+        while(l<r){
+            int m=l+(r-l)/2;
+            if(v[m]>=idx){
+                r=m;
+            }else{
+                l=m+1;
+            }
+        }
+        d=min(d,abs(v[l]-idx));
+        
+        if(l>0) d=min(d,abs(v[l-1]-idx));
+        
+        return d;
+    } 
+    
+    vector<int> shortestDistanceColor(vector<int>& colors, vector<vector<int>>& queries) {
+        vector<int> one, two, three;
+        
+        int n=colors.size();
+        bool c1=false, c2=false, c3=false;
+        for(int i=0;i<n;i++){
+            if(colors[i]==1) one.push_back(i), c1=true;
+            if(colors[i]==2) two.push_back(i), c2=true;
+            if(colors[i]==3) three.push_back(i), c3=true;
         }
         
         vector<int> ans;
