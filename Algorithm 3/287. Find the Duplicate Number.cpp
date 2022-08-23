5d9367a196d75a275525643255a91456e4c047a4
Abhay Singh <abhay5349singh@gmail.com>
2022-08-23 12:18:28 +0530

    Update 287. Find the Duplicate Number.cpp

--- Algorithm 3/287. Find the Duplicate Number.cpp
+++ Algorithm 3/287. Find the Duplicate Number.cpp
@@ -5,42 +5,11 @@ public:
     int findDuplicate(vector<int>& v) {
        int n=v.size(); 
         
-       int x=0, i;
-        for(i=0;i<v.size();i++){
-            x^=v[i];
-            x^=(i+1);
+       for(int i=0;i<n;i++){
+            int idx = abs(v[i])-1;
+            if(v[idx]<0) return idx+1; 
+            v[idx] *= -1;
         }
-        
-        int rmsb = x & (-x);
-        int a=0, b=0;
-        
-        for(i=0;i<v.size();i++){
-            if(v[i] & rmsb){
-                a^=v[i];
-            }else{
-                b^=v[i];
-            }
-            
-            if((i+1) & rmsb){
-                a^=(i+1);
-            }else{
-                b^=(i+1);
-            }
-        }
-        
-        bool f1=false, f2=false;
-        for(i=0;i<v.size();i++){
-            if(a==v[i]){
-                f1=true;
-                break;
-            }
-            if(b==v[i]){
-                f2=true;
-                break;
-            }
-        }
-        cout<<a<<","<<b;
-        if(f1==true) return a;
-        return b;
+        return -1;
     }
 };
