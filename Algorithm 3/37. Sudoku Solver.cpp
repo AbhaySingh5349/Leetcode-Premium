ebaa90783f8c5bb74629d53e90ebcaf52553f055
Abhay Singh <abhay5349singh@gmail.com>
2022-08-24 13:20:27 +0530

    Update 37. Sudoku Solver.cpp

--- Algorithm 3/37. Sudoku Solver.cpp
+++ Algorithm 3/37. Sudoku Solver.cpp
@@ -1,5 +1,7 @@
 Question Link: https://leetcode.com/problems/sudoku-solver/
 
+// Approach 1:
+
 class Solution {
 public:
     map<int,map<char,bool>> row, col, sub;
@@ -42,5 +44,59 @@ public:
             }
         }
         solve(grid,0);
+    }
+};
+
+// Approach 2: Bit-masking
+
+class Solution {
+public:
+    
+    vector<int> row, col, sub;
+    bool solve(vector<vector<char>>& grid, int pos){
+        int n=9;
+        if(pos==n*n) return true; 
+        
+        int i=pos/n, j=pos%n;
+        int g=(i/3)*3+j/3;
+        
+        if(grid[i][j]!='.'){
+            if(solve(grid,pos+1)) return true;
+        }else{
+            for(int d=1;d<=9;d++){
+                if((row[i] & (1<<(d-1)))==0 && (col[j] & (1<<(d-1)))==0 && (sub[g] & (1<<(d-1)))==0){
+                    row[i] |= 1<<(d-1);
+                    col[j] |= 1<<(d-1);
+                    sub[g] |= 1<<(d-1);
+                    grid[i][j]=d+'0';
+                    
+                    if(solve(grid,pos+1)) return true;
+                    
+                    row[i] ^= 1<<(d-1);
+                    col[j] ^= 1<<(d-1);
+                    sub[g] ^= 1<<(d-1);
+                    grid[i][j]='.';
+                }
+            }
+        }
+        return false;
+    }
+    
+    void solveSudoku(vector<vector<char>>& grid) {
+        
+        row.resize(9,0), col.resize(9,0), sub.resize(9,0); 
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(grid[i][j]=='.') continue;
+                
+                int d=grid[i][j]-'0';
+                
+                int g=(i/3)*3+j/3;
+                row[i] |= 1<<(d-1);
+                col[j] |= 1<<(d-1);
+                sub[g] |= 1<<(d-1);
+            }
+        }
+        solve(grid,0); 
     }
 };
