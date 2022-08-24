cb8ecb780fdf1fc0ce43a6e3446d98c5b9d331ad
Abhay Singh <abhay5349singh@gmail.com>
2022-08-24 16:34:50 +0530

    Update 53. Maximum Subarray.cpp

--- Algorithm 3/53. Maximum Subarray.cpp
+++ Algorithm 3/53. Maximum Subarray.cpp
@@ -1,5 +1,7 @@
 Question Link: https://leetcode.com/problems/maximum-subarray/
 
+// Approach 1:
+
 class Solution {
 public:
     int maxSubArray(vector<int>& a) {
@@ -12,6 +14,27 @@ public:
         for(int i=1;i<n;i++){
             dp[i]=max(dp[i-1]+a[i],a[i]);
             ans=max(ans,dp[i]);
+        }
+        return ans;
+    }
+};
+
+// Approach 2:
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& a) {
+        int n=a.size();
+        
+        int ans=a[0];
+        int sum=0;
+        
+        for(int i=0;i<n;i++){
+            sum+=a[i];
+            
+            ans=max(ans,sum);
+            
+            if(sum<0) sum=0;
         }
         return ans;
     }
