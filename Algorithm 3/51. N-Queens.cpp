cc7f3ab3670e1184b8a2478968d8c8694f84dbbb
Abhay Singh <abhay5349singh@gmail.com>
2022-08-24 11:14:29 +0530

    Update 51. N-Queens.cpp

--- Algorithm 3/51. N-Queens.cpp
+++ Algorithm 3/51. N-Queens.cpp
@@ -2,8 +2,8 @@ Question Link: https://leetcode.com/problems/n-queens/
 
 Time complexity: O(N!)
 Unlike the brute force approach, we will only place queens on squares that aren't under attack. For the first queen, we have N options.
-For the next queen, we won't attempt to place it in the same row as the first queen, and there must be at least one square attacked diagonally by the first queen as well. Thus, the maximum number of squares we can consider for the second queen is N - 2.
-For the third queen, we won't attempt to place it in 2 rows already occupied by the first 2 queens, and there must be at least two squares attacked diagonally from the first 2 queens. Thus, the maximum number of squares we can consider for the third queen is N - 4.
+For the next queen, we won't attempt to place it in the same 'row & column' as the first queen, and there must be at least one square attacked diagonally by the first queen as well. Thus, the maximum number of squares we can consider for the second queen is N - 2.
+For the third queen, we won't attempt to place it in 2 'rows & columns' already occupied by the first 2 queens, and there must be at least two squares attacked diagonally from the first 2 queens. Thus, the maximum number of squares we can consider for the third queen is N - 4.
 While it costs O(N^2) to build each valid solution, the amount of valid solutions S(N) does not grow nearly as fast as N!N!, so O(N! + S(N) * N^2)
 
 class Solution {
