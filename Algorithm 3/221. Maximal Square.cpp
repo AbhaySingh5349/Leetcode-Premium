f65fa48971390b5d31632f0c12cea56190425f81
Abhay Singh <abhay5349singh@gmail.com>
2022-08-24 23:08:40 +0530

    Update 221. Maximal Square.cpp

--- Algorithm 3/221. Maximal Square.cpp
+++ Algorithm 3/221. Maximal Square.cpp
@@ -1,5 +1,6 @@
 Question Link: https://leetcode.com/problems/maximal-square/
 
+// Approach 1:
 class Solution {
 public:
     
@@ -29,6 +30,30 @@ public:
                 }
             }
         }
+        return ans*ans;
+    }
+};
+
+// Approach 2:
+class Solution {
+public:
+    
+    int maximalSquare(vector<vector<char>>& grid) {
+        int n=grid.size(), m=grid[0].size();
+        
+        int dp[n][m];
+        int ans=0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(grid[i][j]=='0'){
+                    dp[i][j]=0;
+                }else{
+                    dp[i][j]=1+min((i<n-1 && j<m-1 ? dp[i+1][j+1]:0),min((i<n-1 ? dp[i+1][j]:0),(j<m-1 ? dp[i][j+1]:0)));
+                }
+                ans=max(ans,dp[i][j]);
+            }
+        }
+        
         return ans*ans;
     }
 };
