36d1cfde7a071686022b2c7548ad1b8eca587fda
Abhay Singh <abhay5349singh@gmail.com>
2022-08-24 19:57:24 +0530

    Update 714. Best Time to Buy and Sell Stock with Transaction Fee.cpp

--- Algorithm 3/714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
+++ Algorithm 3/714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
@@ -1,5 +1,41 @@
 Question Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
 
+// Approach 1:
+class Solution {
+public:
+    
+    int solve(vector<int>& prices, int idx, bool prev, int fee, vector<vector<int>> &memo){
+        if(idx>=prices.size()) return 0;
+        
+        if(memo[idx][prev] != -1) return memo[idx][prev];
+        
+        int p1=0, p2=0;
+        
+        // can buy
+        if(prev == false){
+            // decide to buy
+            p1 = solve(prices, idx+1, true, fee, memo) - prices[idx];
+            
+            // skip buying
+            p2 = solve(prices, idx+1, false, fee, memo);
+        }else{
+            // decide to sell
+            p1 = solve(prices, idx+1, false, fee, memo) + prices[idx]-fee;
+            
+            // skip selling
+            p2 = solve(prices, idx+1, prev, fee, memo);
+        }
+        memo[idx][prev] = max(p1,p2);
+        return memo[idx][prev];
+    }
+    
+    int maxProfit(vector<int> &prices, int fee) {
+        vector<vector<int>> memo(prices.size(), vector<int>(2,-1));
+        return solve(prices,0,0,fee,memo);
+    }
+};
+
+// Approach 2:
 class Solution {
 public:
     int maxProfit(vector<int> &a, int fee) {
