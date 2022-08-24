f7492f6a2584c6cf3a386cfede8ff621a2703897
Abhay Singh <abhay5349singh@gmail.com>
2022-08-24 16:32:30 +0530

    Add files via upload

---
+++ Algorithm 3/53. Maximum Subarray.cpp
@@ -0,0 +1,18 @@
+Question Link: https://leetcode.com/problems/maximum-subarray/
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& a) {
+        int n=a.size();
+        
+        int dp[n];
+        dp[0]=a[0];
+        
+        int ans=a[0];
+        for(int i=1;i<n;i++){
+            dp[i]=max(dp[i-1]+a[i],a[i]);
+            ans=max(ans,dp[i]);
+        }
+        return ans;
+    }
+};
