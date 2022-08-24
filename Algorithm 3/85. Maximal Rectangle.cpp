96b655283d9befcd1c31193935f73927f9f2cff6
Abhay Singh <abhay5349singh@gmail.com>
2022-08-24 23:51:38 +0530

    Update 85. Maximal Rectangle.cpp

--- Algorithm 3/85. Maximal Rectangle.cpp
+++ Algorithm 3/85. Maximal Rectangle.cpp
@@ -1,5 +1,6 @@
 Question Link: https://leetcode.com/problems/maximal-rectangle/
 
+// Approach 1:
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& mat) {
@@ -39,6 +40,64 @@ public:
                 }
                 ans=max(ans,curmax);
             }
+        }
+        return ans;
+    }
+};
+
+// Approach 2:
+class Solution {
+public:
+    
+    int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size();
+        
+        stack<pair<int,int>> st;
+        
+        vector<int> nsl(n,-1), nsr(n,n);
+        for(int i=0;i<n;i++){
+            while(st.size()>0 && st.top().first>=heights[i]){
+                nsr[st.top().second]=i;
+                st.pop();
+            }
+            if(st.size()>0) nsl[i]=st.top().second;
+            st.push({heights[i],i});
+        }
+        
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int curh=heights[i];
+            int width=nsr[i]-nsl[i]-1;
+            ans=max(ans,curh*width);
+        }
+        return ans; 
+    }
+    
+    int maximalRectangle(vector<vector<char>>& mat) {
+        int n=mat.size();
+        if(n==0) return 0;
+        int m=mat[0].size();
+        
+        // count of 1s on top for each cell 
+        vector<vector<int>> top(n,vector<int>(m,0));
+        for(int j=0;j<m;j++){
+            int c=0;
+            for(int i=0;i<n;i++){
+                if(mat[i][j]=='1'){
+                    c++;
+                }else{
+                    c=0;
+                }
+                top[i][j]=c;
+            }
+        }
+        
+        int ans=0;
+        
+        for(int i=0;i<n;i++){
+            vector<int> heights=top[i];
+            int area=largestRectangleArea(heights);
+            ans=max(ans,area);
         }
         return ans;
     }
