0112f35be8e91920a58bfc4472e704a7172c6883
Abhay Singh <abhay5349singh@gmail.com>
2022-08-25 14:11:05 +0530

    Update 123. Best Time to Buy and Sell Stock III.cpp

--- Algorithm 3/123. Best Time to Buy and Sell Stock III.cpp
+++ Algorithm 3/123. Best Time to Buy and Sell Stock III.cpp
@@ -1,5 +1,6 @@
 Question Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
 
+// Approach 1:
 class Solution {
 public:
     
@@ -29,5 +30,38 @@ public:
         vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(2,-1)));
         
         return traverseStock(prices, 0, 0, 0, dp);
+    }
+};
+
+// Approach 2:
+class Solution {
+public:
+    int maxProfit(vector<int>& a) {
+        int n=a.size();
+        if(n<2) return 0;
+        
+        int left[n]; // max possible profit if perform 1 transaction from [0,i]
+        left[0]=0;
+        int minl=a[0];
+        for(int i=1;i<n;i++){
+            left[i]=max(left[i-1],a[i]-minl);
+            minl=min(minl,a[i]);
+        }
+        
+        int right[n]; // max possible profit if perform 1 transaction from [i,n-1]
+        right[n-1]=0;
+        int maxr=a[n-1];
+        for(int i=n-2;i>=0;i--){
+            right[i]=max(right[i+1],maxr-a[i]);
+            maxr=max(maxr,a[i]);
+        }
+        
+        int ans=0;
+        for(int i=0;i<n-1;i++){
+            ans=max(ans,left[i]+right[i+1]);
+            ans=max(ans,max(left[i],right[i]));
+        }
+        
+        return ans;
     }
 };
