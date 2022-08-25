bb944be975010bd662a1c02dbe04859a3c7fb819
Abhay Singh <abhay5349singh@gmail.com>
2022-08-25 10:17:58 +0530

    Update 486. Predict the Winner.cpp

--- Algorithm 3/486. Predict the Winner.cpp
+++ Algorithm 3/486. Predict the Winner.cpp
@@ -1,5 +1,6 @@
 Question Link: https://leetcode.com/problems/predict-the-winner/
 
+// Approach 1:
 class Solution {
 public:
     
@@ -24,5 +25,34 @@ public:
         
         vector<vector<int>> memo(n,vector<int>(n,-1));
         return solve(a,0,n-1,memo)>=0;
+    }
+};
+
+// Approach 2:
+class Solution {
+public:
+    
+    int solve(vector<int>& a, int s, int e){
+        int n=a.size();
+        if(s>e) return 0;
+        if(s==e) return a[s];
+        
+        return max(a[s]-solve(a,s+1,e), a[e]-solve(a,s,e-1));
+    }
+    
+    bool PredictTheWinner(vector<int>& a) {
+        int n=a.size();
+        if(n==1) return true;
+        
+        int dp[n][n];
+        for(int i=0;i<n;i++) dp[i][i]=a[i];
+        
+        for(int len=2;len<=n;len++){
+            for(int i=0;i<n-len+1;i++){
+                int j=i+len-1;
+                dp[i][j]=max(a[i]-dp[i+1][j],a[j]-dp[i][j-1]);
+            }
+        }
+        return (dp[0][n-1]>=0);
     }
 };
