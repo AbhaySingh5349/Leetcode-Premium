34b332b02e1a9ce80823b8e8548063f8cd1e6c95
Abhay Singh <abhay5349singh@gmail.com>
2022-08-25 13:27:58 +0530

    Update 983. Minimum Cost For Tickets.cpp

--- Algorithm 3/983. Minimum Cost For Tickets.cpp
+++ Algorithm 3/983. Minimum Cost For Tickets.cpp
@@ -1,5 +1,40 @@
 Question Link: https://leetcode.com/problems/minimum-cost-for-tickets/
 
+// Approach 1:
+class Solution {
+public:
+    
+    int memo[501];
+    set<int> st;
+    
+    int val(vector<int>& days, vector<int>& cost, int n, int idx){
+        if(idx>days[n-1]) return 0;
+        
+        if(memo[idx]!=-1) return memo[idx];
+        
+        int ans=INT_MAX;
+        if(st.find(idx)==st.end()){
+            ans=val(days,cost,n,idx+1);
+        }else{
+            ans=min(ans,cost[0]+val(days,cost,n,idx+1));
+            ans=min(ans,cost[1]+val(days,cost,n,idx+7));
+            ans=min(ans,cost[2]+val(days,cost,n,idx+30));
+        }
+        
+        return memo[idx]=ans;
+    }
+    
+    int mincostTickets(vector<int>& days, vector<int>& cost) {
+        memset(memo,-1,sizeof(memo));
+        int n=days.size();
+        
+        for(int i=0;i<n;i++) st.insert(days[i]);
+        
+        return val(days,cost,n,days[0]);
+    }
+};
+
+// Approach 2:
 class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& cost) {
