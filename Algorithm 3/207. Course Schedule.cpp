9a4a28eba99276449eca4c84a90d781cad6638ff
Abhay Singh <abhay5349singh@gmail.com>
2022-08-25 21:08:34 +0530

    Update 207. Course Schedule.cpp

--- Algorithm 3/207. Course Schedule.cpp
+++ Algorithm 3/207. Course Schedule.cpp
@@ -1,5 +1,6 @@
 Question Link: https://leetcode.com/problems/course-schedule/
 
+// Approach 1: (Topo-sort)
 class Solution {
 public:
     
@@ -31,5 +32,41 @@ public:
             }
         }
         return (st.size()==n);
+    }
+};
+
+// Approach 2: (Topo-sort)
+class Solution {
+public:
+    
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<bool> visited(n,false);
+        vector<int> graph[n];
+        vector<int> indegree(n,0);
+        for(int i=0;i<pre.size();i++){
+            int u=pre[i][0], v=pre[i][1];
+            graph[v].push_back(u);
+            indegree[u]++;
+        }
+        
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        int c=0;
+        while(q.size()>0){
+            int src=q.front();
+            q.pop();
+            c++;
+            
+            for(int i=0;i<graph[src].size();i++){
+                int child=graph[src][i];
+                indegree[child]--;
+                if(indegree[child]==0) q.push(child);
+            }
+        }
+        return (c==n);
     }
 };
