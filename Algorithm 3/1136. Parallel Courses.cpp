f9bc9b5d36cfd0b40f2df7e3849191302e9949ec
Abhay Singh <abhay5349singh@gmail.com>
2022-08-26 08:44:18 +0530

    Update 1136. Parallel Courses.cpp

--- Algorithm 3/1136. Parallel Courses.cpp
+++ Algorithm 3/1136. Parallel Courses.cpp
@@ -1,5 +1,6 @@
 Question Link: https://leetcode.com/problems/parallel-courses/
 
+// Approach 1:
 class Solution {
 public:
     
@@ -52,5 +53,42 @@ public:
             }
         }
         return ans;
+    }
+};
+
+// Approach 2: Topo-sort
+class Solution {
+public:
+    
+    int minimumSemesters(int n, vector<vector<int>> &edges) {
+        vector<int> graph[n];
+        vector<int> indegree(n,0);
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0], v=edges[i][1];
+            graph[u-1].push_back(v-1);
+            indegree[v-1]++;
+        }
+        
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        
+        int c=0;
+        while(q.size()>0){
+            int size=q.size();
+            n-=size;
+            while(size--){
+                int u=q.front();
+                q.pop();
+                
+                for(int v:graph[u]){
+                    indegree[v]--;
+                    if(indegree[v]==0) q.push(v);
+                }
+            }
+            c++;
+        }
+        return (n==0 ? c:-1);
     }
 };
