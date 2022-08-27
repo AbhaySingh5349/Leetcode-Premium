7c87f16eb25388645af0d02f556ec7be2c82430c
Abhay Singh <abhay5349singh@gmail.com>
2022-08-27 13:37:37 +0530

    Add files via upload

---
+++ SQL/511. Game Play Analysis I.cpp
@@ -0,0 +1,49 @@
+Question Link: https://leetcode.com/problems/game-play-analysis-i/
+
+SQL Schema
+Table: Activity
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+(player_id, event_date) is the primary key of this table.
+This table shows the activity of players of some games.
+Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+ 
+
+Write an SQL query to report the first login date for each player.
+
+Input: 
+Activity table:
++-----------+-----------+------------+--------------+
+| player_id | device_id | event_date | games_played |
++-----------+-----------+------------+--------------+
+| 1         | 2         | 2016-03-01 | 5            |
+| 1         | 2         | 2016-05-02 | 6            |
+| 2         | 3         | 2017-06-25 | 1            |
+| 3         | 1         | 2016-03-02 | 0            |
+| 3         | 4         | 2018-07-03 | 5            |
++-----------+-----------+------------+--------------+
+Output: 
++-----------+-------------+
+| player_id | first_login |
++-----------+-------------+
+| 1         | 2016-03-01  |
+| 2         | 2017-06-25  |
+| 3         | 2016-03-02  |
++-----------+-------------+
+
+// Approach 1:
+SELECT player_id,SUBSTR(MIN(event_date), 0, 10) as first_login 
+FROM Activity 
+GROUP BY player_id;
+
+// Approach 2:
+SELECT player_id, TO_CHAR(MIN(event_date), 'YYYY-MM-DD') as first_login
+FROM Activity
+GROUP BY player_id
