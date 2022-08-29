c1bf37f6f09e2228e58d60abecfa674a2684fcfc
Abhay Singh <abhay5349singh@gmail.com>
2022-08-29 20:10:19 +0530

    Add files via upload

---
+++ MySQL/1077. Project Employees III.cpp
@@ -0,0 +1,84 @@
+Question Link: https://leetcode.com/problems/project-employees-iii/
+
+SQL Schema
+Table: Project
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| project_id  | int     |
+| employee_id | int     |
++-------------+---------+
+(project_id, employee_id) is the primary key of this table.
+employee_id is a foreign key to Employee table.
+Each row of this table indicates that the employee with employee_id is working on the project with project_id.
+ 
+Table: Employee
+
++------------------+---------+
+| Column Name      | Type    |
++------------------+---------+
+| employee_id      | int     |
+| name             | varchar |
+| experience_years | int     |
++------------------+---------+
+employee_id is the primary key of this table.
+Each row of this table contains information about one employee.
+ 
+
+Write an SQL query that reports the most experienced employees in each project. In case of a tie, report all employees with the maximum number of experience years.
+
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key of this table.
+Each row of this table indicates the product name of each product.
+
+Write an SQL query that reports the product_name, year, and price for each sale_id in the Sales table.
+
+Input: 
+Project table:
++-------------+-------------+
+| project_id  | employee_id |
++-------------+-------------+
+| 1           | 1           |
+| 1           | 2           |
+| 1           | 3           |
+| 2           | 1           |
+| 2           | 4           |
++-------------+-------------+
+Employee table:
++-------------+--------+------------------+
+| employee_id | name   | experience_years |
++-------------+--------+------------------+
+| 1           | Khaled | 3                |
+| 2           | Ali    | 2                |
+| 3           | John   | 3                |
+| 4           | Doe    | 2                |
++-------------+--------+------------------+
+Output: 
++-------------+---------------+
+| project_id  | employee_id   |
++-------------+---------------+
+| 1           | 1             |
+| 1           | 3             |
+| 2           | 1             |
++-------------+---------------+
+Explanation: Both employees with id 1 and 3 have the most experience among the employees of the first project. 
+For the second project, the employee with id 1 has the most experience.
+
+// Approach : Using commom table expression
+WITH cte as(
+    SELECT p.project_id, p.employee_id, e.experience_years, MAX(e.experience_years) OVER (PARTITION BY p.project_id) as max_exp
+    FROM Project p LEFT JOIN Employee e
+    ON p.employee_id = e.employee_id
+)
+
+SELECT project_id, employee_id 
+FROM cte
+WHERE (experience_years = max_exp)
