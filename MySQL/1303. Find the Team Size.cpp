0a3e4df3cca98f1613fa3ad844fa38e64b37f012
Abhay Singh <abhay5349singh@gmail.com>
2022-09-02 00:19:56 +0530

    Add files via upload

---
+++ MySQL/1303. Find the Team Size.cpp
@@ -0,0 +1,47 @@
+Question Link: https://leetcode.com/problems/find-the-team-size/
+
+SQL Schema
+Table: Employee
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| employee_id   | int     |
+| team_id       | int     |
++---------------+---------+
+employee_id is the primary key for this table.
+Each row of this table contains the ID of each employee and their respective team.
+ 
+Write an SQL query to find the team size of each of the employees.
+
+Input: 
+Employee Table:
++-------------+------------+
+| employee_id | team_id    |
++-------------+------------+
+|     1       |     8      |
+|     2       |     8      |
+|     3       |     8      |
+|     4       |     7      |
+|     5       |     9      |
+|     6       |     9      |
++-------------+------------+
+Output: 
++-------------+------------+
+| employee_id | team_size  |
++-------------+------------+
+|     1       |     3      |
+|     2       |     3      |
+|     3       |     3      |
+|     4       |     1      |
+|     5       |     2      |
+|     6       |     2      |
++-------------+------------+
+Explanation: 
+Employees with Id 1,2,3 are part of a team with team_id = 8.
+Employee with Id 4 is part of a team with team_id = 7.
+Employees with Id 5,6 are part of a team with team_id = 9.
+
+// Approach : Using Window function
+SELECT employee_id, COUNT(*) OVER (PARTITION BY team_id) as team_size
+FROM Employee
