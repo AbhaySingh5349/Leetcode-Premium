c3a6abb1c997ddaed58e4127995d61770596806b
Abhay Singh <abhay5349singh@gmail.com>
2022-09-15 19:38:05 +0530

    Add files via upload

---
+++ MySQL/183. Customers Who Never Order.cpp
@@ -0,0 +1,62 @@
+Question Link: https://leetcode.com/problems/customers-who-never-order/
+
+SQL Schema
+Table: Customers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID and name of a customer.
+ 
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key column for this table.
+customerId is a foreign key of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+ 
+Write an SQL query to report all customers who never order anything.
+
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+// Approach 1: Using Join
+SELECT name as Customers
+FROM Customers c LEFT JOIN Orders o
+ON c.Id = o.CustomerId
+WHERE o.CustomerId IS NULL
+
+// Approach 2: Using sub-queries
+SELECT name as Customers FROM Customers WHERE id NOT IN (SELECT customerId FROM Orders)
