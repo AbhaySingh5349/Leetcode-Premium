2494ba2c50a3a9d00ddd89ea3a7439a233f21c41
Abhay Singh <abhay5349singh@gmail.com>
2022-09-26 18:25:46 +0530

    Update 516. Longest Palindromic Subsequence.cpp

--- Dynamic Programming/516. Longest Palindromic Subsequence.cpp
+++ Dynamic Programming/516. Longest Palindromic Subsequence.cpp
@@ -1,6 +1,5 @@
 Question Link: https://leetcode.com/problems/longest-palindromic-subsequence/
 
-
 // Approach 1:
 class Solution {
 public:
@@ -52,5 +51,35 @@ public:
         }
         
         return dp[0][n-1];
+    }
+};
+
+
+// Approach 3:
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n=s.length();
+        
+        string s1=s;
+        reverse(s.begin(),s.end());
+        string s2=s;
+        
+        int dp[n+1][n+1];
+        for(int i=0;i<n;i++){
+            dp[i][0]=0;
+            dp[0][i]=0;
+        }
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
     }
 };
