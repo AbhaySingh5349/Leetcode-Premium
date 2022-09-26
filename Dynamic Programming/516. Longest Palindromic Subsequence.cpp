f5d0bab5386d7f5ff7d6bbbf3aaa5fc1c701226d
Abhay Singh <abhay5349singh@gmail.com>
2022-09-26 17:54:31 +0530

    Add files via upload

--- Dynamic Programming/516. Longest Palindromic Subsequence.cpp
+++ Dynamic Programming/516. Longest Palindromic Subsequence.cpp
@@ -33,6 +33,7 @@ public:
 // Approach 2:
 class Solution {
 public:
+    
     int longestPalindromeSubseq(string s) {
         int n=s.length();
         
@@ -51,35 +52,5 @@ public:
         }
         
         return dp[0][n-1];
-    }
-};
-
-
-// Approach 3:
-class Solution {
-public:
-    int longestPalindromeSubseq(string s) {
-        int n=s.length();
-        
-        string s1=s;
-        reverse(s.begin(),s.end());
-        string s2=s;
-        
-        int dp[n+1][n+1];
-        for(int i=0;i<n;i++){
-            dp[i][0]=0;
-            dp[0][i]=0;
-        }
-        
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<n+1;j++){
-                if(s1[i-1]==s2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
-        }
-        return dp[n][n];
     }
 };
