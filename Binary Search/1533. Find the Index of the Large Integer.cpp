0f095a29509096f4094a4d4ff38394f2010f1a69
Abhay Singh <abhay5349singh@gmail.com>
2022-10-02 19:18:48 +0530

    Update 1533. Find the Index of the Large Integer.cpp

--- Binary Search/1533. Find the Index of the Large Integer.cpp
+++ Binary Search/1533. Find the Index of the Large Integer.cpp
@@ -1,7 +1,7 @@
 Question Link: https://leetcode.com/problems/find-the-index-of-the-large-integer/
 
 
-// Approach :
+// Approach 1:
 
 /**
  * // This is the ArrayReader's API interface.
@@ -41,5 +41,49 @@ public:
             }
         }
         return -1;
+    }
+};
+
+
+// Approach 2:
+
+/**
+ * // This is the ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class ArrayReader {
+ *   public:
+ *     // Compares the sum of arr[l..r] with the sum of arr[x..y] 
+ *     // return 1 if sum(arr[l..r]) > sum(arr[x..y])
+ *     // return 0 if sum(arr[l..r]) == sum(arr[x..y])
+ *     // return -1 if sum(arr[l..r]) < sum(arr[x..y])
+ *     int compareSub(int l, int r, int x, int y);
+ *
+ *     // Returns the length of the array
+ *     int length();
+ * };
+ */
+
+class Solution {
+public:
+    int getIndex(ArrayReader &reader) {
+        int l=0, r=reader.length()-1;
+
+        while(l<r){
+            int m=l+(r-l)/2;
+            
+            int lLen=m-l+1, rLen=r-m;
+            int limit= (lLen > rLen ? m-1:m); // for equal length comparison 
+            
+            int val=reader.compareSub(l,max(limit,l),min(m+1,r),r);
+            
+            if(val==1){
+                r=limit;
+            }else if(val==-1){
+                l=limit+1;
+            }else{
+                return m;
+            }
+        }
+        return l;
     }
 };
