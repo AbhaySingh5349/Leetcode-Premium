6540249875209d23900523472280c2462341ce7b
Abhay Singh <abhay5349singh@gmail.com>
2022-10-03 07:31:24 +0530

    Update 1011. Capacity To Ship Packages Within D Days.cpp

--- Binary Search/1011. Capacity To Ship Packages Within D Days.cpp
+++ Binary Search/1011. Capacity To Ship Packages Within D Days.cpp
@@ -1,7 +1,7 @@
 Question Link: https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
 
 
-// Approach :
+// Approach 1:
 class Solution {
 public:
     
@@ -10,6 +10,42 @@ public:
         
         int c=1, s=a[0];
         for(int i=1;i<n;i++){
+            s+=a[i];
+            if(s>maxWeight){
+                s=a[i];
+                c++;
+            }
+        }
+        return (c<=days);
+    }
+    
+    int shipWithinDays(vector<int>& a, int days) {
+        int left = *max_element(a.begin(),a.end());
+        int right = accumulate(a.begin(),a.end(),0);
+        
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(possible(a,days,mid)){
+                right=mid;
+            }else{
+                left=mid+1;
+            }
+        }
+        return left;
+    }
+};
+
+
+
+// Approach 2:
+class Solution {
+public:
+    
+    bool possible(vector<int>& a, int days, int maxWeight){
+        int n=a.size();
+        
+        int c=1, s=0;
+        for(int i=0;i<n;i++){
             s+=a[i];
             if(s>maxWeight){
                 s=a[i];
