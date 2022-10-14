719f14ab58e4516bf2620aafc747e7580491571e
Abhay Singh <abhay5349singh@gmail.com>
2022-10-14 06:51:53 +0530

    Update 737. Sentence Similarity II.cpp

--- Graphs/737. Sentence Similarity II.cpp
+++ Graphs/737. Sentence Similarity II.cpp
@@ -5,23 +5,23 @@ Question Link: https://leetcode.com/problems/sentence-similarity-ii/
 class Solution {
 public:
     
-    string find(map<string, string>& mp, string s) {
-        if(mp.find(s) == mp.end()) return mp[s]=s;
-        if(mp[s]==s) return s;
-        return mp[s]=find(mp,mp[s]);
+    string find(map<string, string>& parent, string s) {
+        if(parent.find(s) == parent.end()) return parent[s]=s;
+        if(parent[s]==s) return s;
+        return parent[s]=find(parent,parent[s]);
     }
     
     bool areSentencesSimilarTwo(vector<string>& a, vector<string>& b, vector<vector<string>>& pairs) {
         if (a.size() != b.size()) return false;
         
-        map<string, string> mp;
+        map<string, string> parent;
         for (auto p : pairs) {
-            string pu = find(mp, p[0]), pv = find(mp, p[1]);
-            if (pu != pv) mp[pv] = pu;
+            string pu = find(parent, p[0]), pv = find(parent, p[1]);
+            if (pu != pv) parent[pv] = pu;
         }
-
+        
         for (int i = 0; i < a.size(); i++){
-            if(a[i] != b[i] && find(mp, a[i]) != find(mp, b[i])) return false;
+            if(a[i] != b[i] && find(parent, a[i]) != find(parent, b[i])) return false;
         }
 
         return true;
