44749f0547c0624b8269267b31f6b565a8621829
Abhay Singh <abhay5349singh@gmail.com>
2022-11-03 00:06:10 +0530

    Add files via upload

---
+++ Data Structures/Graph/1319. Number of Operations to Make Network Connected.cpp
@@ -0,0 +1,45 @@
+Question Link: https://leetcode.com/problems/number-of-operations-to-make-network-connected/
+
+
+// Approach :                                            
+class Solution {
+public:
+    
+    int find(vector<int> &parent, int u){
+        if(parent[u]==u) return u;
+        return parent[u]=find(parent,parent[u]);
+    }
+    
+    void merge(vector<int> &parent, vector<int> &rk, int pu, int pv){
+        if(rk[pu]>=rk[pv]){
+            rk[pu]+=rk[pv];
+            parent[pv]=pu;
+        }else{
+            rk[pv]+=rk[pu];
+            parent[pu]=pv;
+        }
+    }
+    
+    int makeConnected(int n, vector<vector<int>>& edges) {
+        if(edges.size()<n-1) return -1;
+        
+        vector<int> parent(n), rk(n);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            rk[i]=1;
+        }
+        
+        int cc=n;
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0], v=edges[i][1];
+            int pu=find(parent,u), pv=find(parent,v);
+            
+            if(pu!=pv){
+                cc--;
+                merge(parent,rk,pu,pv);
+            }
+        }
+        
+        return (cc-1);
+    }
+};
